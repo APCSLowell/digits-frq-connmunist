9ab1e97cdd395b4d8aef5f6cce7fb5324c53ffcd
connor ng <143060779+connmunist@users.noreply.github.com>
2024-04-13 17:45:44 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -8,24 +8,29 @@ public class Digits
 	public Digits(int num)
 	{ 
 	digitList = new ArrayList<Integer>();
-	if(num == 0)
-	digitList.add(new Integer(0)):
+if (num == 0)
+{
 
-	while(num > 0){
-		digitList.add(0, new Integer(num % 10)):
-		num /= 0;
-	}    
+digitList.add(new Integer(0));
+}
+
+while (num > 0)
+{
+digitList.add(0, new Integer(num % 10));
+num /= 10;
+} 
 }
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-	for(int i =0 ; i < digitList.size()=1);i++){
-		if(digitList.get(i).intValue() >= digitList.get(i+1).intValue()){
-			return false;
-			
-		}
-		}
-	return true;
+	for (int i = 0; i < digitList.size()-1; i++)
+{
+if (digitList.get(i).intValue() >= digitList.get(i+1).intValue())
+{
+return false;
+}
+}
+return true;
 	}
 	
 	public String toString()
